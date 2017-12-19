0669209747b271c0de47faabd7df01caba9aca2d
Sander Koenders <sanderkoenders@gmail.com>
2017-12-19 20:43:34 +0100

    Chore(Initial): Initial commit with project files

---
+++ stryker.conf.js
@@ -0,0 +1,18 @@
+module.exports = function(config) {
+  config.set({
+    files: [
+      '!src/**/*.ts',
+      '!**/*.d.ts',
+      { pattern: 'src/**/*.ts', included: false, mutated: true },
+      '!./src/index.ts'
+    ],
+    testRunner: "mocha",
+    testFramework: "mocha",
+    mutator: "typescript",
+    transpilers: ["typescript"],
+    reporter: ["clear-text", "progress", "html"],
+    tsconfigFile: "tsconfig.json",
+    coverageAnalysis: "off",
+    logLevel: "info"
+  });
+};

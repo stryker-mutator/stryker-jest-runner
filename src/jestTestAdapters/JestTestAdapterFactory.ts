b2bf9936661c1fe91ac10e083799a562fee95b79
Sander Koenders <sanderkoenders@gmail.com>
2017-12-21 15:32:32 +0100

    Feat(Factory): Add implementation for JestTestAdapterFactory

---
+++ src/jestTestAdapters/JestTestAdapterFactory.ts
@@ -0,0 +1,37 @@
+import JestTestAdapter from "./JestTestAdapter";
+import JestPromiseAdapter from './JestPromiseTestAdapter';
+import JestCallbackAdapter from './JestCallbackTestAdapter';
+import * as semver from 'semver';
+
+export default class JestTestAdapterFactory {
+  public static getJestTestAdapter(loader?: NodeRequire): JestTestAdapter {
+    let jestTestAdapter: JestTestAdapter;
+
+    switch(this.getJestType(loader)) {
+      case 'promise': 
+        jestTestAdapter = new JestPromiseAdapter;
+      break;
+      case 'callback':
+        jestTestAdapter = new JestCallbackAdapter;
+      break;
+      default:
+        throw new Error('You need Jest version >= 20.0.0 to use Stryker');
+    }
+
+    return jestTestAdapter;
+  }
+
+  private static getJestType(loader?: NodeRequire): 'promise' | 'callback' | 'invalid' {
+    loader = loader || require;
+
+    const jestVersion = loader('jest/package.json').version;
+
+    if(semver.satisfies(jestVersion, '<20.0.0')) {
+      return 'invalid';
+    } else if(semver.satisfies(jestVersion, '>=20.0.0 <21.0.0')) {
+      return 'callback';
+    } else {
+      return 'promise';
+    }
+  }
+}

86baf737c2156afdc1bad537b38310c3e54391d0
Sander Koenders <sanderkoenders@gmail.com>
2017-12-21 09:44:49 +0100

    Test(JestConfigEditor): Add test case for react projects

--- test/unit/JestConfigEditorSpec.ts
+++ test/unit/JestConfigEditorSpec.ts
@@ -3,18 +3,23 @@ import JestConfigEditor from '../../src/JestConfigEditor';
 import { Config } from 'stryker-api/config';
 import * as sinon from 'sinon';
 import { assert, expect } from 'chai';
+import ReactConfigLoader, * as reactConfigLoader from '../../src/configLoaders/ReactConfigLoader';
 
 describe('JestConfigEditor', () => {
   let jestConfigEditor: JestConfigEditor;
   let sandbox: sinon.SinonSandbox;
 
   let defaultConfigLoaderStub: ConfigLoaderStub;
+  let reactConfigLoaderStub: ConfigLoaderStub;
 
   beforeEach(() => {
     sandbox = sinon.sandbox.create();
 
     defaultConfigLoaderStub = sinon.createStubInstance(DefaultConfigLoader);
+    reactConfigLoaderStub = sinon.createStubInstance(ReactConfigLoader);
+    
     sandbox.stub(defaultConfigLoader, 'default').returns(defaultConfigLoaderStub);
+    sandbox.stub(reactConfigLoader, 'default').returns(reactConfigLoaderStub);
 
     jestConfigEditor = new JestConfigEditor;
   });
@@ -27,6 +32,15 @@ describe('JestConfigEditor', () => {
     jestConfigEditor.edit(config);
 
     assert(defaultConfigLoaderStub.loadConfig.calledOnce, 'DefaultConfigLoader loadConfig not called');
+  });
+
+  it('should call the reactConfigLoader edit method when no project is defined', () => {
+    const config = new Config;
+    config.set({ jest: { project: 'react' }});
+
+    jestConfigEditor.edit(config);
+
+    assert(reactConfigLoaderStub.loadConfig.calledOnce, 'ReactConfigLoader loadConfig not called');
   });
 
   it('should return an error when an invalid project is defined', () => {

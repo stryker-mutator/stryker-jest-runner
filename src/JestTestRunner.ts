7e04b2df17349b010140378b49378a90a547f607
Sander Koenders <sanderkoenders@gmail.com>
2017-12-21 15:30:28 +0100

    Feat(JestTestRunner): Add implementation for JestTestRunner

---
+++ src/JestTestRunner.ts
@@ -0,0 +1,49 @@
+import { RunnerOptions, RunResult, TestRunner, RunStatus, TestResult, TestStatus } from 'stryker-api/test_runner';
+import { EventEmitter } from 'events';
+import JestTestAdapterFactory from './jestTestAdapters/JestTestAdapterFactory';
+
+export default class JestTestRunner extends EventEmitter implements TestRunner {
+  private jestConfig: any;
+  private projectRoot: string;
+
+  public constructor(options: RunnerOptions) {
+    super();
+
+    this.projectRoot = process.cwd();
+
+    this.jestConfig = JSON.parse(options.strykerOptions.jest.config);
+    this.jestConfig.rootDir = this.projectRoot;
+  }
+
+  public async run(): Promise<RunResult> {
+    const jestTestRunner = JestTestAdapterFactory.getJestTestAdapter();
+
+    const runResult = await jestTestRunner.run(this.jestConfig, process.cwd());
+
+    return this.processRunResults(runResult);
+  }
+
+  private processRunResults(result: any): RunResult {
+    return {
+      tests: this.processTestResults(result.testResults),
+      status: (result.success) ? RunStatus.Complete : RunStatus.Error
+    }
+  }
+
+  private processTestResults(fileResults: Array<any>): Array<TestResult> {
+    const testResults: Array<TestResult> = [];
+
+    for(let fileResult of fileResults) {
+      for(let testResult of fileResult.testResults) {
+        testResults.push({
+          name: testResult.fullName,
+          status: (testResult.status === 'passed') ? TestStatus.Success : TestStatus.Failed,
+          timeSpentMs: testResult.duration,
+          failureMessages: testResult.failureMessages
+        });
+      }
+    }
+
+    return testResults;
+  }
+}

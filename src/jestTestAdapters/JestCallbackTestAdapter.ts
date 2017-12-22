d849d2929dea4db0191cd162d7ecb73d714d37f4
Sander Koenders <sanderkoenders@gmail.com>
2017-12-22 12:32:31 +0100

    Feat(TestAdapters): Add implementation for test adapters

--- src/jestTestAdapters/JestCallbackTestAdapter.ts
+++ src/jestTestAdapters/JestCallbackTestAdapter.ts
@@ -1,7 +1,23 @@
 import JestTestAdapter from './JestTestAdapter';
 
 export default class JestCallbackTestAdapter implements JestTestAdapter {
-  public run(config: Object, projectRoot: string): Promise<any> {
-    throw new Error('Not yet implemented');
+  private testRunner: any;
+  
+  public constructor(loader?: NodeRequire) {
+    loader = loader || require;
+
+    this.testRunner = loader('jest');
+  }
+
+  public run(config: any, projectRoot: string): Promise<any> {
+    config.reporters = [];
+
+    return new Promise((resolve) => {
+      this.testRunner.runCLI({ 
+        config: JSON.stringify(config), 
+        runInBand: true,
+        silent: true
+      }, [projectRoot], resolve);
+    });
   }
 }
--- src/jestTestAdapters/JestPromiseTestAdapter.ts
+++ src/jestTestAdapters/JestPromiseTestAdapter.ts
@@ -1,7 +1,21 @@
 import JestTestAdapter from './JestTestAdapter';
 
 export default class JestPromiseTestAdapter implements JestTestAdapter {
-  public run(config: Object, projectRoot: string): Promise<any> {
-    throw new Error('Not yet implemented');
+  private testRunner: any;
+  
+  public constructor(loader?: NodeRequire) {
+    loader = loader || require;
+
+    this.testRunner = loader('jest').runCLI;
+  }
+
+  public run(config: any, projectRoot: string): Promise<any> {
+    config.reporters = [];
+
+    return this.testRunner({
+      config: JSON.stringify(config),
+      runInBand: true,
+      silent: true
+    }, [projectRoot]);
   }
 }

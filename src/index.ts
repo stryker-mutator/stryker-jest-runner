c712568743f348abb04ec19cd91b211cea8c6ea4
Sander Koenders <sanderkoenders@gmail.com>
2018-01-08 09:55:03 +0100

    Chore(delete): Delete old version of jest-test-runner

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { TestRunnerFactory } from 'stryker-api/test_runner';
-import JestTestRunner from './JestTestRunner';
-
-// This is the main file loaded when stryker loads this plugin
-// Report your plugin to the correct Factory
-
-TestRunnerFactory.instance().register('jest', JestTestRunner);

60bc8cbc95ec24c03795623e176dea5f542e326a
Sander Koenders <sanderkoenders@gmail.com>
2018-01-08 18:23:39 +0100

    Docs(ConfigLoader): Add documentation to the interface

--- src/configLoaders/ConfigLoader.ts
+++ src/configLoaders/ConfigLoader.ts
@@ -1,3 +1,16 @@
+/**
+ * The Configloader interface is used to load different kinds of configurations for Jest.
+ * Custom ConfigLoaders should implement this interface, the ConfigEditor will then be able to use it to load a Jest configuration.
+ * 
+ * ConfigLoaders are typically used for projects that do not provide their configuration via the package.json file (e.g. React).
+ * The loaderConfig method will return a usable config for Jest to use.
+ */
+
 export default interface ConfigLoader {
+  /*
+   * Load the JSON representation of a Jest Configuration.
+   *
+   * @return {string} a JSON object containing the Jest configuration.
+   */
   loadConfig(): string;
 }

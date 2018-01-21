1132ce91b1f2c8a6a783ce04576f9cea1881db1e
Sander Koenders <sanderkoenders@gmail.com>
2018-01-21 14:20:12 +0100

    Feat(Mocha): Update to mocha 5

--- src/JestConfigEditor.ts
+++ src/JestConfigEditor.ts
@@ -4,13 +4,15 @@ import ConfigLoader from './configLoaders/ConfigLoader';
 import * as fs from 'fs';
 import ReactConfigLoader from './configLoaders/ReactConfigLoader';
 
+const DEFAULT_PROJECT_NAME = 'default';
+
 export default class JestConfigEditor implements ConfigEditor {
   public edit(strykerConfig: Config): void {
     // If there is no Jest property on the Stryker config create it
     strykerConfig.jest = strykerConfig.jest || {};
 
     // When no project is set set it to 'default'
-    strykerConfig.jest.project = strykerConfig.jest.project || 'default';
+    strykerConfig.jest.project = strykerConfig.jest.project || DEFAULT_PROJECT_NAME;
 
     // When no config property is set load the configuration with the project type
     strykerConfig.jest.config = strykerConfig.jest.config || this.getConfigLoader(strykerConfig.jest.project).loadConfig();
@@ -20,7 +22,7 @@ export default class JestConfigEditor implements ConfigEditor {
     let configLoader: ConfigLoader;
 
     switch (project.toLowerCase()) {
-      case 'default':
+      case DEFAULT_PROJECT_NAME:
         configLoader = new DefaultConfigLoader(process.cwd(), fs);
       break;
       case 'react':

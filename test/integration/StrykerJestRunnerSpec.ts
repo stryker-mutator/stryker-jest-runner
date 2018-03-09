29f64d677f535be5b33244e1182676232d62f6be
Sander Koenders <sanderkoenders@gmail.com>
2018-03-09 09:03:15 +0100

    Refactor(cleanup): Move matchTestResults to the test itselve
    Also clean up some code and make the code clearer overall

--- test/integration/StrykerJestRunnerSpec.ts
+++ test/integration/StrykerJestRunnerSpec.ts
@@ -1,19 +1,16 @@
 import JestConfigEditor from '../../src/JestConfigEditor';
 import { Config } from 'stryker-api/config';
-import { RunnerOptions, RunStatus, TestStatus, TestResult } from 'stryker-api/test_runner';
+import { RunnerOptions, RunResult, RunStatus, TestResult, TestStatus } from 'stryker-api/test_runner';
 import * as sinon from 'sinon';
 import { expect } from 'chai';
 import JestTestRunner from '../../src/JestTestRunner';
 import * as path from 'path';
-import matchTestResult from '../unit/utils/matchTestResult';
-
-// Get the project root, we will be stub process.cwd later on
-const jestProjectRoot = process.cwd();
 
 // Needed for Jest in order to run tests
 process.env.BABEL_ENV = 'test';
 
-const FIRST_TEST = 0;
+// Get the project root, we will be stub process.cwd later on
+const jestProjectRoot = process.cwd();
 
 describe('Integration StrykerJestRunner', function () {
   // Set timeout for integration tests to 10 seconds for travis
@@ -23,16 +20,6 @@ describe('Integration StrykerJestRunner', function () {
   let runOptions: RunnerOptions;
   let processCwdStub: sinon.SinonStub;
   let sandbox: sinon.SinonSandbox;
-
-  // Partial of the expected test results
-  const partialExpectedTestResults: Array<Partial<TestResult>> = [
-    { name: 'Add should be able to add two numbers', status: TestStatus.Success },
-    { name: 'Add should be able to add one to a number', status: TestStatus.Failed },
-    { name: 'Add should be able negate a number', status: TestStatus.Success },
-    { name: 'Add should be able to recognize a negative number', status: TestStatus.Success },
-    { name: 'Add should be able to recognize that 0 is not a negative number', status: TestStatus.Success },
-    { name: 'Circle should have a circumference of 2PI when the radius is 1', status: TestStatus.Failed }
-  ];
 
   beforeEach(() => {
     sandbox = sinon.createSandbox();
@@ -53,6 +40,7 @@ describe('Integration StrykerJestRunner', function () {
   it('should run tests on the example react project', async () => {
     processCwdStub.returns(getProjectRoot('reactProject'));
     runOptions.strykerOptions.set({ jest: { project: 'react' } });
+    const FIRST_TEST = 0;
 
     jestConfigEditor.edit(runOptions.strykerOptions as Config);
 
@@ -71,57 +59,59 @@ describe('Integration StrykerJestRunner', function () {
 
   it('should run tests on the example custom project using package.json', async () => {
     processCwdStub.returns(getProjectRoot('exampleProject'));
-
     jestConfigEditor.edit(runOptions.strykerOptions as Config);
     const jestTestRunner = new JestTestRunner(runOptions);
 
     const result = await jestTestRunner.run();
 
-    expect(result).to.have.property('tests');
-    expect(result.tests).to.be.an('array').with.length(partialExpectedTestResults.length);
-
-    for (const test of result.tests) {
-      expect(partialExpectedTestResults.map(testResult => testResult.name)).to.include(test.name);
-
-      const expectedTestResult = matchTestResult(test, partialExpectedTestResults);
-
-      if (expectedTestResult) {
-        expect(test.status).to.equal(expectedTestResult.status);
-        expect(test.timeSpentMs).to.be.above(-1);
-        expect(test.failureMessages).to.be.an('array');
-      }
-    }
-
-    expect(result.status).to.equal(RunStatus.Complete);
+    assertExampleProjectRunResult(result);
   });
 
   it('should run tests on the example custom project using jest.config.js', async () => {
     processCwdStub.returns(getProjectRoot('exampleProjectWithExplicitJestConfig'));
-
     jestConfigEditor.edit(runOptions.strykerOptions as Config);
     const jestTestRunner = new JestTestRunner(runOptions);
 
     const result = await jestTestRunner.run();
 
-    expect(result).to.have.property('tests');
-    expect(result.tests).to.be.an('array').with.length(partialExpectedTestResults.length);
-
-    for (const test of result.tests) {
-      expect(partialExpectedTestResults.map(testResult => testResult.name)).to.include(test.name);
-
-      const expectedTestResult = matchTestResult(test, partialExpectedTestResults);
-
-      if (expectedTestResult) {
-        expect(test.status).to.equal(expectedTestResult.status);
-        expect(test.timeSpentMs).to.be.above(-1);
-        expect(test.failureMessages).to.be.an('array');
-      }
-    }
-
-    expect(result.status).to.equal(RunStatus.Complete);
+    assertExampleProjectRunResult(result);
   });
 });
 
 function getProjectRoot(testResource: string) {
   return path.join(jestProjectRoot, 'testResources', testResource);
+}
+
+function assertExampleProjectRunResult(result: RunResult) {
+  const expectedPartialTestResults: Array<Partial<TestResult>> = [
+    { name: 'Add should be able to add two numbers', status: TestStatus.Success },
+    { name: 'Add should be able to add one to a number', status: TestStatus.Failed },
+    { name: 'Add should be able negate a number', status: TestStatus.Success },
+    { name: 'Add should be able to recognize a negative number', status: TestStatus.Success },
+    { name: 'Add should be able to recognize that 0 is not a negative number', status: TestStatus.Success },
+    { name: 'Circle should have a circumference of 2PI when the radius is 1', status: TestStatus.Failed }
+  ];
+
+  expect(result).to.have.property('tests');
+  expect(result.tests).to.be.an('array').with.length(expectedPartialTestResults.length);
+
+  for (const test of result.tests) {
+    expect(test.name).to.be.oneOf(expectedPartialTestResults.map(testResult => testResult.name));
+
+    // We already determined that the test is one of the tests specified in the partialExpectedTestResults array
+    // matchTestResult cannot return undefined because of this, cast to TestResult unconditionally.
+    const expectedTestResult = matchTestResult(test, expectedPartialTestResults) as TestResult;
+
+    expect(test.status).to.equal(expectedTestResult.status);
+    expect(test.timeSpentMs).to.be.above(-1);
+    expect(test.failureMessages).to.be.an('array');
+  }
+
+  expect(result.status).to.equal(RunStatus.Complete);
+}
+
+function matchTestResult(testResult: TestResult, expectedTestResults: Array<Partial<TestResult>>): Partial<TestResult> | undefined {
+  return expectedTestResults.find(expectedTestResult => {
+      return expectedTestResult.name === testResult.name;
+  });
 }
--- test/unit/utils/matchTestResult.ts
+++
@@ -1,8 +0,0 @@
-import { TestResult } from 'stryker-api/test_runner';
-
-export default (testResult: TestResult, expectedTestResults: Array<Partial<TestResult>>): Partial<TestResult> | undefined => {
-    return expectedTestResults.find(expectedTestResult => {
-        return expectedTestResult.name === testResult.name;
-    });
-};
-

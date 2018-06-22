fcffaceb5f961f33e5da293ca942ab2dde3d3948
Sander Koenders <sanderkoenders@gmail.com>
2018-06-22 16:20:39 +0200

    Feat(Types): Return RunResult when calling a TestAdapter

--- src/jestTestAdapters/JestPromiseTestAdapter.ts
+++ src/jestTestAdapters/JestPromiseTestAdapter.ts
@@ -1,11 +1,11 @@
 import JestTestAdapter from './JestTestAdapter';
 import { getLogger } from 'log4js';
-import { Configuration, runCLI } from 'jest';
+import { Configuration, runCLI, RunResult } from 'jest';
 
 export default class JestPromiseTestAdapter implements JestTestAdapter {
   private log = getLogger(JestPromiseTestAdapter.name);
 
-  public run(jestConfig: Configuration, projectRoot: string): Promise<any> {
+  public run(jestConfig: Configuration, projectRoot: string): Promise<RunResult> {
     jestConfig.reporters = [];
     const config = JSON.stringify(jestConfig);
     this.log.trace(`Invoking Jest with config ${config}`);
